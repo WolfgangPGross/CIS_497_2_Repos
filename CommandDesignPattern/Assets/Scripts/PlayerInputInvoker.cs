957c081f33c1957bf305507c185c1bf56f851839
Wolfgang Gross <60198468+WolfgangPGross@users.noreply.github.com>
2021-03-21 23:20:00 -0500

    Add

---
+++ CommandDesignPattern/Assets/Scripts/PlayerInputInvoker.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/*
+ * Wolfgang Gross
+ * CIS 497 2
+ * 
+ * Command Design Pattern
+ * 
+ * Invoker
+ * */
+
+public class PlayerInputInvoker : MonoBehaviour
+{
+    public MoveObject moveObject;
+    private Command moveForward;
+    private Command moveBackward;
+    private Command rotateRight;
+    private Command rotateLeft;
+
+    //A stack of commands to keep track of the history of commands
+    public Stack<Command> commandHistory;
+
+    // Initialize commands and our stack of commands using Awake or Start
+    void Awake()
+    {
+        moveForward = new MoveForward(moveObject);
+        moveBackward = new MoveBackward(moveObject);
+        rotateLeft = new RotateLeft(moveObject);
+        rotateRight = new RotateRight(moveObject);
+
+        commandHistory = new Stack<Command>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            moveForward.Execute();
+            commandHistory.Push(moveForward);
+        }
+
+        if (Input.GetKey(KeyCode.S))
+        {
+            moveBackward.Execute();
+            commandHistory.Push(moveBackward);
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            rotateLeft.Execute();
+            commandHistory.Push(rotateLeft);
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            rotateRight.Execute();
+            commandHistory.Push(rotateRight);
+        }
+
+        if (Input.GetKey(KeyCode.Q))
+        {
+            if (commandHistory.Count != 0)
+            {
+                //pop the last command off our stack
+                Command lastCommand = commandHistory.Pop();
+
+                //call Undo() on the last command
+                lastCommand.Undo();
+            }
+        }
+
+
+    }
+}
---
+++ CommandDesignPattern/Assets/Scripts/RotateLeft.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateLeft : Command
+{
+    MoveObject moveObject;
+
+    Stack<Vector3> positionHistory;
+    Stack<Quaternion> rotationHistory;
+    Stack<Vector3> velocityHistory;
+
+    public RotateLeft(MoveObject moveObject)
+    {
+        this.moveObject = moveObject;
+        positionHistory = new Stack<Vector3>();
+        rotationHistory = new Stack<Quaternion>();
+        velocityHistory = new Stack<Vector3>();
+    }
+
+    public void Execute()
+    {
+        positionHistory.Push(moveObject.GetCurrentPosition());
+        rotationHistory.Push(moveObject.GetCurrentRotation());
+        velocityHistory.Push(moveObject.GetCurrentVelocity());
+
+        moveObject.RotateLeft();
+
+    }
+
+    public void Undo()
+    {
+        //consider Undoing the move left towards a move to the right...
+        //moveObject.MoveRight();
+
+        //Assign the Vector3 position in our positionHistory stack to our gameObject
+        if (positionHistory.Count != 0)
+        {
+            moveObject.gameObject.transform.rotation = rotationHistory.Pop();
+            moveObject.gameObject.transform.position = positionHistory.Pop();
+            moveObject.gameObject.GetComponent<Rigidbody>().velocity = velocityHistory.Pop();
+        }
+    }
+}
